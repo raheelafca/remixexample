9ae0a640d3dd6a211ac3d0163405f83bacf63944
raheelwali <raheel@afcamail.com>
2024-02-02 10:40:42 -0600

    updates

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,14 +1,23 @@
+import { 
+  Sandpack,
+  SandpackProvider, 
+  SandpackLayout, 
+  SandpackCodeEditor, 
+  SandpackFileExplorer 
+} from "@codesandbox/sandpack-react";
+import { cobalt2 } from "@codesandbox/sandpack-themes";
+
 export const meta = () => {
   return [
     { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { name: "description", content: "Welcome to Remix Hello!" },
   ];
 };
 
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Welcome to Remix Hello</h1>
       <ul>
         <li>
           <a
@@ -34,6 +43,29 @@ export default function Index() {
           </a>
         </li>
       </ul>
+
+      {/* <Sandpack 
+      theme={cobalt2}
+        template="react" 
+        files={{
+          '/Wrapper.js': `export default () => "";`,
+          '/Button.js': {
+            code: `export default () => {
+              return <button>Hello</button>
+            };`,
+            readOnly: true, // Set as non-editable, defaults to `false`
+            active: true, // Set as main file, defaults to `false`
+            hidden: false // Tab visibility, defaults to `false`
+          }
+        }}
+      />; */}
+
+<SandpackProvider theme={cobalt2} template="react">
+    <SandpackLayout>
+      <SandpackFileExplorer />
+      <SandpackCodeEditor />
+    </SandpackLayout>
+  </SandpackProvider>
     </div>
   );
 }
